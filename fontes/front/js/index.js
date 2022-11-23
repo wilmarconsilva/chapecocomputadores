b037fc6240151917923c51cf8f68f2f9b1bf8b24
wilmac2002 <wilson.marconxbox@hotmail.com>
2022-11-23 20:09:12 -0300

    v5

--- fontes/front/js/index.js
+++ fontes/front/js/index.js
@@ -27,7 +27,7 @@ categorys.set('CPU', 'Processadores');
 categorys.set('RAM', 'Memórias');
 categorys.set('GAB', 'Gabinetes');
 
-function showProducts()
+function showMostSelledProducts()
 {
 
     for (let i = 0; i < products.length; i++) 
@@ -39,7 +39,7 @@ function showProducts()
 
         //link img
         var link_img = document.createElement('a');
-        link_img.href = 
+        link_img.href = '#'
         
         link_img.id = product[i].prod_id;
         link_img.onclick = function (e)
@@ -62,18 +62,102 @@ function showProducts()
         p.classList.add('product-category');
         p.innerHTML = categorys.get(products[i].prod_type);
         //nome
-        var name
+        var name = document.createElement('h3');
+        name.classList.add('product-name');
+        link_name = document.createElement('a');
+        link_name.href = '#'
+        link_name.innerHTML = product[i].prod_desc;
+        name.appendChild(link_name);
+        //preço
+        price = document.createElement('h4');
+        price.classList.add('product-price');
+        price.innerHTML = prod_price;
+        //append
+        div_body.appendChild(p);
+        div_body.appendChild(name);
+        div_body.appendChild(price);
 
+        //div add to cart
+        var div_addCart = document.createElement('div');
+        div_addCart.classList.add('add-to-cart');
+        //button
+        var btn = document.createElement('button');
+        btn.classList.add('add-to-cart-btn');
+        btn.innerHTML ='Colocar no carrrinho';
+        var i_btn = document.createElement('i');
+        i_btn.classList.add('fa fa-shopping-cart');
+        //append
+        div_addCart.appendChild(btn);
+        btn.appendChild(i_btn);
 
-
-
-
-        
-
-
-        
     }
 
+}
 
+function showNewProducts()
+{
+
+    for (let i = 0; i < 3; i++) 
+    {
+        var random = Math.random() * products.length;
+        //div principal
+        var product = document.createElement("div");
+        product.classList.add("product");
+
+
+        //link img
+        var link_img = document.createElement('a');
+        link_img.href = '#'
+        
+        link_img.id = product[random].prod_id;
+        link_img.onclick = function (e)
+        {
+            details(this.id)
+        }
+        //img
+        var img = document.createElement("img");
+        img.src = product[random].prod_img;
+        img.classList.add("product-img");
+        //append
+        link_img.appendChild(img);
+        product.appendChild(link_img);
+
+        //div body
+        var div_body = document.createElement('div');
+        div_body.classList.add('product-body');
+        //categoria
+        var p = document.createElement('p');
+        p.classList.add('product-category');
+        p.innerHTML = categorys.get(products[random].prod_type);
+        //nome
+        var name = document.createElement('h3');
+        name.classList.add('product-name');
+        link_name = document.createElement('a');
+        link_name.href = '#'
+        link_name.innerHTML = product[random].prod_desc;
+        name.appendChild(link_name);
+        //preço
+        price = document.createElement('h4');
+        price.classList.add('product-price');
+        price.innerHTML = prod_price;
+        //append
+        div_body.appendChild(p);
+        div_body.appendChild(name);
+        div_body.appendChild(price);
+
+        //div add to cart
+        var div_addCart = document.createElement('div');
+        div_addCart.classList.add('add-to-cart');
+        //button
+        var btn = document.createElement('button');
+        btn.classList.add('add-to-cart-btn');
+        btn.innerHTML ='Colocar no carrrinho';
+        var i_btn = document.createElement('i');
+        i_btn.classList.add('fa fa-shopping-cart');
+        //append
+        div_addCart.appendChild(btn);
+        btn.appendChild(i_btn);
+
+    }
 
 }
