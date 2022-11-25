00303fe3f154c6541e8e5361e1789020fb2bd983
wilmac2002 <wilson.marconxbox@hotmail.com>
2022-11-25 00:06:59 -0300

    v5

--- fontes/front/js/index.js
+++ fontes/front/js/index.js
@@ -35,7 +35,8 @@ function showMostSelledProducts()
 
         //link img
         var link_img = document.createElement('a');
-        link_img.href = '#'
+        link_img.href = '#';
+        link_img.classList.add('product-img');
         
         link_img.id = products[i].prod_id;
         link_img.onclick = function (e)
@@ -44,10 +45,11 @@ function showMostSelledProducts()
         }
         //img
         var img = document.createElement("img");
-        img.src = products[i].prod_img;
+        img.src = '/fontes/front/img/' + products[i].imgpath + '.jpg';
         img.classList.add("product-img");
         //append
         link_img.appendChild(img);
+        //append pai
         product.appendChild(link_img);
 
 
@@ -68,12 +70,12 @@ function showMostSelledProducts()
         //preço
         price = document.createElement('h4');
         price.classList.add('product-price');
-        price.innerHTML = products[i].prod_price;
+        price.innerHTML = 'R$ ' + products[i].prod_preco + ',99';
         //append
         div_body.appendChild(p);
         div_body.appendChild(name);
         div_body.appendChild(price);
-
+        //append pai
         product.appendChild(div_body);
 
         //div add to cart
@@ -89,76 +91,94 @@ function showMostSelledProducts()
         //append
         div_addCart.appendChild(btn);
         btn.appendChild(i_btn);
-
+        //append pai
         product.appendChild(div_addCart);
+
+        //append master
+        div_mostSelledProducts.appendChild(product);
 
     }
 
 }
+
+//limpar lançamentos
+div_newProducts = document.getElementById('products-slick-2');
+div_newProducts.innerHTML = '';
 
 function showNewProducts()
 {
 
     for (let i = 0; i < 3; i++) 
     {
-        var random = Math.random() * products.length;
+        var random = Math.floor(Math.random() * products.length);
+
         //div principal
         var product = document.createElement("div");
         product.classList.add("product");
 
 
-        //link img
-        var link_img = document.createElement('a');
-        link_img.href = '#'
-        
-        link_img.id = product[random].prod_id;
-        link_img.onclick = function (e)
-        {
-            details(this.id)
-        }
-        //img
-        var img = document.createElement("img");
-        img.src = product[random].prod_img;
-        img.classList.add("product-img");
-        //append
-        link_img.appendChild(img);
-        product.appendChild(link_img);
-
-        //div body
-        var div_body = document.createElement('div');
-        div_body.classList.add('product-body');
-        //categoria
-        var p = document.createElement('p');
-        p.classList.add('product-category');
-        p.innerHTML = categorys.get(products[random].prod_type);
-        //nome
-        var name = document.createElement('h3');
-        name.classList.add('product-name');
-        link_name = document.createElement('a');
-        link_name.href = '#'
-        link_name.innerHTML = product[random].prod_desc;
-        name.appendChild(link_name);
-        //preço
-        price = document.createElement('h4');
-        price.classList.add('product-price');
-        price.innerHTML = prod_price;
-        //append
-        div_body.appendChild(p);
-        div_body.appendChild(name);
-        div_body.appendChild(price);
-
-        //div add to cart
-        var div_addCart = document.createElement('div');
-        div_addCart.classList.add('add-to-cart');
-        //button
-        var btn = document.createElement('button');
-        btn.classList.add('add-to-cart-btn');
-        btn.innerHTML ='Colocar no carrrinho';
-        var i_btn = document.createElement('i');
-        i_btn.classList.add('fa fa-shopping-cart');
-        //append
-        div_addCart.appendChild(btn);
-        btn.appendChild(i_btn);
+         //link img
+         var link_img = document.createElement('a');
+         link_img.href = '#'
+         link_img.classList.add('product-img')
+         
+         link_img.id = products[random].prod_id;
+         link_img.onclick = function (e)
+         {
+             details(this.id)
+         }
+         //img
+         var img = document.createElement("img");
+         img.src = '/fontes/front/img/' + products[random].imgpath + '.jpg';
+         //append
+         link_img.appendChild(img);
+         //append pai
+         product.appendChild(link_img);
+ 
+ 
+         //div body
+         var div_body = document.createElement('div');
+         div_body.classList.add('product-body');
+         //categoria
+         var p = document.createElement('p');
+         p.classList.add('product-category');
+         p.innerHTML = categorys.get(products[random].prod_type);
+         //nome
+         var name = document.createElement('h3');
+         name.classList.add('product-name');
+         link_name = document.createElement('a');
+         link_name.href = '#'
+         link_name.innerHTML = products[random].prod_desc;
+         name.appendChild(link_name);
+         //preço
+         price = document.createElement('h4');
+         price.classList.add('product-price');
+         price.innerHTML = 'R$ ' + products[random].prod_preco + ',99';
+         //append
+         div_body.appendChild(p);
+         div_body.appendChild(name);
+         div_body.appendChild(price);
+         //append pai
+         product.appendChild(div_body);
+ 
+         //div add to cart
+         var div_addCart = document.createElement('div');
+         div_addCart.classList.add('add-to-cart');
+         //button
+         var btn = document.createElement('button');
+         btn.classList.add('add-to-cart-btn');
+         btn.innerHTML ='Colocar no carrrinho';
+         var i_btn = document.createElement('i');
+         i_btn.classList.add('fa');
+         i_btn.classList.add('fa-shopping-cart');
+         //append
+         div_addCart.appendChild(btn);
+         btn.appendChild(i_btn);
+         //append pai
+         product.appendChild(div_addCart);
+ 
+         //append master
+         div_newProducts.appendChild(product);
 
     }
 
